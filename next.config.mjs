dddc60e10b11f618ca8fe1e3265eb3d8916952f6
mahdii <mahdizaltnii@gmail.com>
2024-01-19 16:58:43 +0100

    refactor:changing next config

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    images: {
+      remotePatterns: [
+        {
+          protocol: "https",
+          hostname: "api.getwemap.com",
+          pathname: "**",
+        },
+      ],
+    },
+  };
+  
+  module.exports = nextConfig;
